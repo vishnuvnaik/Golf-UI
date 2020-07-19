6e4be9899c42a8912e5ff417ca640f3ff0dc24ca
= <=>
2020-07-20 01:05:56 +0530

    [Vishnu]Refactor.added slide show button functionality

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,11 +1,44 @@
 $(document).ready(function () {
   $("#flip").click(function () {
     $(".heading").hide();
+    $(".container").hide();
     $("#panel").slideDown("slow");
   });
+  $("#crossBtn").click(function () {
+    $("#panel").slideUp("slow");
+    $(".container").show();
+    $(".heading").show();
+  });
 });
+var slideIndex = 1;
+showSlides(slideIndex);
 
-$("#crossBtn").click(function () {
-  $("#panel").slideUp("slow");
-  $(".heading").show();
-});
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
+}
